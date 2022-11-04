492a31f0635dd4676dab7ca9775ed7a5f1fe54f7
Sam Arita <arita.sam@student.moringaschool.com>
2022-11-04 15:31:25 +0300

    Completed assignment

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,11 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+    #actor = Actor.find_by(id: self.actor_id)
+    # Actor.find(self.actor_id)
+    #puts actor[:first_name]
+   "#{self.name} always says: #{self.catchphrase}"
+ end
 end
